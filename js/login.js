c63e9bc7a1904778f2debafa7fa8a758781f98a2
Joa Marron <marronjoana@gmail.com>
2022-08-24 22:27:39 -0300

    Utilizando JSON y Storage

---
+++ js/login.js
@@ -0,0 +1,43 @@
+let recordar = document.getElementById("rememberMe");
+let btnLogin = document.getElementById("btnLogin");
+let btnVaciarLocalStorage = document.getElementById("btnVaciarLocalStorage");
+let btnVaciarSessionStorage = document.getElementById("btnVaciarSessionStorage");
+
+function guardarDatos(storage) {
+    let user = document.getElementById('emailAdress').value;
+    let pass = document.getElementById('password').value;
+
+    const usuario = {
+        'user': user,
+        'pass': pass,
+    }
+
+    if (storage === 'sessionStorage') {
+        sessionStorage.setItem('user', JSON.stringify(usuario));
+    }
+    if (storage === 'localStorage') {
+        localStorage.setItem('user', JSON.stringify(usuario));
+    }
+
+}
+
+function borrarDatos(storage) {
+    storage.clear()
+}
+
+btnLogin.addEventListener('click', () => {
+    if (recordar.checked) {
+        guardarDatos('localStorage');
+    } else {
+        guardarDatos('sessionStorage');
+    }
+})
+
+btnVaciarLocalStorage.addEventListener('click', () => {
+    borrarDatos(localStorage);
+    borrarDatos(sessionStorage);
+})
+
+btnVaciarSessionStorage.addEventListener('click', () => {
+    borrarDatos(sessionStorage);
+})
