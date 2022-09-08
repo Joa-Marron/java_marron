24d3e6e1f1a2333c3ef1a0d3e77b540a7de55a83
Joa Marron <marronjoana@gmail.com>
2022-09-08 02:07:32 -0300

    Creando base de datos

---
+++ js/salonDeLaFama.js
@@ -0,0 +1,21 @@
+const jsonBtn = document.getElementById('jsonBtn');
+
+function cargarJSON () {
+    fetch('../js/base.json')
+    .then(function(resultado){
+        return resultado.json();
+    })
+    .then(function(data){
+        listaHTML = '';
+        data.forEach(function(animal){
+            listaHTML += `
+            <li>Nombre:${animal.nombre} Especie:${animal.especie} Género:${animal.genero} Edad:${animal.edad}</li>
+            <br>
+            `;
+        })
+        document.getElementById('famosos').innerHTML = listaHTML;
+    })
+    .catch(err => console.error(err));
+}
+
+jsonBtn.addEventListener('click',cargarJSON);
