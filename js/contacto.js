17740c8d80c2f3c34b6c3776a98e53470bd0b267
Joa Marron <marronjoana@gmail.com>
2022-09-15 15:44:17 -0300

    Page Contacto terminada

---
+++ js/contacto.js
@@ -0,0 +1,85 @@
+const infoForm = document.querySelector('#formulario');
+const recordar = document.querySelector('#rememberMe');
+
+
+function guardarDatos() {
+    let username = document.querySelector('#nombre').value
+    let userFname = document.querySelector('#apellido').value
+
+    const usuario = {
+        name: username,
+        fName: userFname
+    }
+
+    localStorage.setItem('user', JSON.stringify(usuario));
+
+}
+
+
+infoForm.addEventListener('submit', function handleSubmit(event) {
+    event.preventDefault();
+    let name = document.querySelector('#nombre').value
+    let firstName = document.querySelector('#apellido').value
+    let number = document.querySelector('#numero').value
+    let mensaje = document.querySelector('#mensaje').value
+
+    const person = {
+
+        name,
+
+        firstName,
+
+        number,
+
+        mensaje
+
+    }
+    localStorage.setItem('data', JSON.stringify(person));
+    infoForm.reset();
+    
+})
+
+recordar.addEventListener('click', () => {
+    Swal.fire({
+        title: '¿Deseas guardar tus datos?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, seguro',
+        cancelButtonText: 'No, no quiero'
+    }).then((result)=>{
+        if(result.isConfirmed){
+            guardarDatos();
+            Swal.fire({
+                title: 'Tus datos se guardaron exitosamente',
+                icon: 'success'
+            })
+        }
+    })
+})
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+let limite = 120;
+const miTexto = document.querySelector('.texto');
+const resultado = document.getElementById('resul');
+resultado.textContent = `o/${limite}`;
+
+function cambiarClases(elemento, claseA, claseB) {
+    elemento.classList.add(claseA);
+    elemento.classList.remove(claseB);
+
+}
+
+function textArea(cajaTexto, cajaLimite, claseA, claseB) {
+    const textLength = miTexto.value.length;
+    cajaLimite.textContent = `${textLength}/${limite}`
+
+    textLength > limite ? cambiarClases(cajaTexto, claseA, claseB) : cambiarClases(cajaTexto, claseB, claseA);
+
+}
+
+
+miTexto.addEventListener('input', () => {
+    textArea(miTexto, resultado, 'sobrepasa', 'noSobrepasa');
+})
--- js/salonDeLaFama.js
+++ js/salonDeLaFama.js
@@ -1,22 +1,38 @@
 const jsonBtn = document.getElementById('jsonBtn');
 
-function cargarJSON () {
+function cargarJSON() {
     fetch('../js/base.json')
-    .then(function(resultado){
-        return resultado.json();
-    })
-    .then(function(data){
-        listaHTML = '';
-        data.forEach(function(animal){
-            listaHTML += `
-            <li>Nombre:${animal.nombre} Especie:${animal.especie} Género:${animal.genero} Edad:${animal.edad}</li>
+        .then(function (resultado) {
+            return resultado.json();
+        })
+        .then(function (data) {
+            listaHTML = '';
+            data.forEach(function (animal) {
+                listaHTML += `
+            <li>Nombre:${animal.nombre} <br> Especie:${animal.especie}<br> Género:${animal.genero}<br> Edad:${animal.edad}</li>
             <br>
             <img src="${animal.img}">
             `;
+            })
+            document.getElementById('famosos').innerHTML = listaHTML;
         })
-        document.getElementById('famosos').innerHTML = listaHTML;
-    })
-    .catch(err => console.error(err));
+        .catch(err => console.error(err));
 }
 
-jsonBtn.addEventListener('click',cargarJSON);
+jsonBtn.addEventListener('click', cargarJSON);
+
+/*---------------------------------------------------------------------------------*/
+
+fetch('../js/base.json')
+    .then(response => responde.json())
+    .then(data => {
+        pintarCards(data)
+    })
+
+function pintarCards(data) {
+    let fakeSeleccion = document.getElementById('jsonBtn')
+    data.forEach(animal => {
+        fakeSeleccion.innerHTML += `<li>Nombre:${animal.nombre} <br> Especie:${animal.especie}<br> Género:${animal.genero}<br> Edad:${animal.edad}</li>
+        <br>`
+    });
+}
